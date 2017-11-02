950c9694bc6cc6422863d3cbf4c55ea68a67aa10
Unknown <jsandrade1998@gmail.com>
2017-11-02 09:29:59 +0100

    Comit Layouts Con Menu

--- app/src/main/java/com/example/jstalin/layouts/MainActivity.java
+++ app/src/main/java/com/example/jstalin/layouts/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jstalin.layouts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,23 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public void boton1(View view){
+        Intent i = new Intent(this, RealtiveLayout1.class);
+        startActivity(i);
+    }
+
+    public void boton2(View view){
+        Intent i = new Intent(this, TableLayout1.class);
+        startActivity(i);
+    }
+
+
+    public void boton3(View view){
+        Intent i = new Intent(this, LinearLayout1.class);
+        startActivity(i);
+    }
+
+
+
 }
